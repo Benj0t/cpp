f1dc5d954afd2dbf06eb300c1306d8176e1519f5
Benjot <bmoreau89000@gmail.com>
2021-12-27 02:57:03 +0100

    setup .hpp + .cpp files

---
+++ cpp02/ex01/main.cpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*	                                                                        */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bemoreau <bemoreau@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/25 14:50:38 by bemoreau          #+#    #+#             */
+/*   Updated: 2021/12/25 14:50:38 by bemoreau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+#include <iostream>
+
+int main(void) 
+{
+    Fixed a;
+    Fixed b(a);
+    Fixed c;
+    c = b;
+    std::cout << a.getRawBits() << std::endl;
+    std::cout << b.getRawBits() << std::endl;
+    std::cout << c.getRawBits() << std::endl;
+    return 0;
+}
